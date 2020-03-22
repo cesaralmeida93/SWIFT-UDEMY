1b54656edba021b7c154c812e3c90118dae1cc9a
cesar <cesar.almeida@gsw.com.br>
2020-03-22 17:37:33 -0300

    ˜finalizado modulo basico de swift

--- CursoIOS.playground/Contents.swift
+++ CursoIOS.playground/Contents.swift
@@ -54,9 +54,64 @@ import UIKit
 
 //Desafio
 
-var frases: [String] = []
+//var frases: [String] = []
+//
+//frases.append("Olá")
+//frases.append("Adeus")
+//frases.append("corona virus")
+//print(frases)
 
-frases.append("Olá")
-frases.append("Adeus")
-frases.append("corona virus")
-print(frases)
+//Set
+//
+//var lista = Set<String>()
+//
+//lista.insert("pizza")
+//lista.insert("Suco")
+//lista.insert("Refrigerante")
+//
+//print(lista)
+
+//Dicionarios
+//
+//var animais = [Int: String]()
+//
+//animais[1] = "animal branco que hiberna"
+//animais[2] = "Melhor amigo do homem"
+//
+//print(animais[2]!)
+
+//var mes = [Int: String]()
+//mes[1] = "Janeiro"
+//mes[2] = "Fevereiro"
+//mes[3] = "Março"
+//mes[4] = "Abril"
+//mes[5] = "Maio"
+//mes[6] = "Junho"
+//mes[7] = "Julho"
+//mes[8] = "Agosto"
+//mes[9] = "Setembto"
+//mes[10] = "Outubro"
+//mes[11] = "Novembro"
+//mes[12] = "Dezembro"
+//
+//print(mes[10]!)
+
+//if else
+
+var preco: Int
+var resultado: String
+
+preco = 500
+resultado = "Nenhuma categoria"
+
+if preco >= 100 && preco <= 200 {
+    resultado = "Carros populares"
+} else if (preco > 200 && preco <= 300){
+    resultado = "Carros médios"
+}else if (preco > 300 && preco <= 400) {
+    resultado = "Carros luxuosos"
+} else {
+    resultado = "Eike batista"
+}
+
+print(resultado)
