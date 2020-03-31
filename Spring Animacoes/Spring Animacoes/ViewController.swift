95ad09b062f3040572d9c2399fee973fe3ddfbe0
cesar <cesar.almeida@gsw.com.br>
2020-03-31 18:51:23 -0300

    entendendo gerenciamento de dependencias

---
+++ Spring Animacoes/Spring Animacoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spring Animacoes
+//
+//  Created by GSW Mobile Team on 31/03/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
